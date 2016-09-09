2c9542954ba2a8c0ed92cf1dc78af3f70064edf6
sagittaria <shuyang_x@hotmail.com>
2016-09-09 20:05:49 +0800

    an ugly-easy display of one's articles

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Articles</div>
+
+                <div class="panel-body">
+                    show all articles here.<br>
+                    @if(count($articles) >0)
+                    <ul>
+                        @foreach ($articles as $article)
+                        <li>
+                            <h4> {{ $article->title }} </h4>
+                            <p> {{ $article->content }} </p>
+                        </li>
+                        @endforeach
+                    </ul>
+                    @else
+                    <p>No article yet.</p>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
