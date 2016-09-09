b912ceb9e1b86836bb7fd45f4ac0616d8f0632b7
sagittaria <shuyang_x@hotmail.com>
2016-09-09 19:58:20 +0800

    add relation: User hasMany Articles

--- app/User.php
+++ app/User.php
@@ -23,4 +23,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get all of the articles for the user.
+     */
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
