d4944653072d0634fad13d080eb52be1a9dc1a03
kasoya <shuyang_x@hotmail.com>
2016-09-25 22:15:43 +0800

    调整文章详情页

--- resources/views/articles/detail.blade.php
+++ resources/views/articles/detail.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h2 class="text-center"> {{ $article->title }} </h2>
-    <div class="row">
+    <div class="row" >
     <div class="col-md-4 text-center"><span class="glyphicon glyphicon-tags"></span>&nbsp;&nbsp;标签</div>
     <div class="col-md-4 text-center"><span class="glyphicon glyphicon-folder-open"></span>&nbsp;&nbsp;分类</div>
     <div class="col-md-4 text-center"><span class="glyphicon glyphicon-time"></span> {{ substr($article->updated_at,0,-3) }}</div>
