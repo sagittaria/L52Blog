20bed618706c401fa826d7d3921b38b6c373af44
kasoya <shuyang_x@hotmail.com>
2016-09-25 22:14:18 +0800

    调整文章列表页

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,39 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Articles</div>
+
 @if(Session::has('success'))
-{{ Session::get('success') }}
-@endif
-                <div class="panel-body">
-                    admin: show all articles here.<br>
-                    @if(count($articles) >0)
-                    <ul>
-                        @foreach ($articles as $article)
-                        <li>
-                            <h4><a href="{{ action('ArticleController@detail',[$article->id]) }}" > {{ $article->title }} </a>
-                                <a href="{{ action('AdminController@edit',[$article->id]) }}">编辑</a>
-                                <form action="{{ action('AdminController@destroy',[$article->id]) }}" method="POST" style="display: inline;">  
-                                    {{ method_field('DELETE') }}
-                                    {{ csrf_field() }}
-                                    <button type="submit">删除</button>
-                                </form>
-                            </h4>
-                            <p> {{ $article->content }} </p>
-                        </li>
-                        @endforeach
-                    </ul>
-                    @else
-                    <p>No article yet.</p>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+  {{ Session::get('success') }}
 </div>
+@endif
+    @if(count($articles) >0)
+    <ul style="list-style-type:none;padding:0;">
+        @foreach ($articles as $article)
+        <li style="border-bottom:1px solid #99ccff;">
+            <h4><a href="{{ action('ArticleController@detail',[$article->id]) }}" > {{ $article->title }} </a>
+                <form action="{{ action('AdminController@destroy',[$article->id]) }}" method="POST" style="display: inline;">  
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                    <button type="submit" class="btn btn-xs btn-danger pull-right">删除</button>
+                </form>
+                <a href="{{ action('AdminController@edit',[$article->id]) }}" class="btn btn-xs btn-primary pull-right" style="margin-right:7px;">编辑</a>
+            </h4>
+            <p>
+            @if(strlen($article->content)>165)
+              {{ substr($article->content,0,165) }}...
+            @else
+              {{  $article->content }}
+            @endif
+            </p>
+        </li>
+        @endforeach
+    </ul>
+    @else
+    <p>No article yet.</p>
+    @endif
 @endsection
 
